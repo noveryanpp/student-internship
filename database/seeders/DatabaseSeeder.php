04bd5fc2782fa0792be0104ddcd286be55351486
Noveryan Putra <noveryanpp@gmail.com>
2025-06-03 08:27:19 +0700

    Production 1.3.2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             StudentRoleSeeder::class,
-            TeacherRoleSeeder::class,
         ]);
     }
 }
